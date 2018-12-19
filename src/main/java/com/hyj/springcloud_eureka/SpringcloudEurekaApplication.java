32fff77f188dd6e9faa01922147f5506c5907a21
mozhumz <554501252@qq.com>
2018-12-19 09:49:16 +0800

    feat:springcloud注册中心搭建

---
+++ src/main/java/com/hyj/springcloud_eureka/SpringcloudEurekaApplication.java
@@ -0,0 +1,18 @@
+package com.hyj.springcloud_eureka;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringcloudEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudEurekaApplication.class, args);
+	}
+
+}
+
