3d604580c3e511994af399d4ae2ffbf9503ebca2
mozhumz <554501252@qq.com>
2019-10-11 22:04:05 +0800

    配置优先级测试

---
+++ src/test/java/com/hyj/eureka/test/TestCase.java
@@ -0,0 +1,15 @@
+package com.hyj.eureka.test;
+
+import org.junit.Test;
+
+public class TestCase {
+    @Test
+    public void test(){
+        Integer a=-128;
+        Integer b=-128;
+        Integer c=128;
+        Integer d=128;
+        System.out.println(a==b);
+        System.out.println(c==d);
+    }
+}
